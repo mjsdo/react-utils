5e9aefcfe66af01281bfc4dd14d9765432c0cf3c
mjsdo <bcad1591@gmail.com>
2024-01-25 15:18:23 +0900

    chore: eslint configs 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,20 @@ module.exports = {
   },
   ignorePatterns: ['dist', '.eslintrc.js'],
   parser: '@typescript-eslint/parser',
+  plugins: ['react'],
   overrides: [
     {
       files: ['**/*.{j,t}s?(x)'],
-      extends: ['plugin:@mjsdo/common'],
+      extends: [
+        'plugin:@mjsdo/common',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+      ],
     },
     {
       files: ['**/*.ts?(x)'],
       extends: ['plugin:@mjsdo/typescript'],
-    },
-    {
-      files: ['**/*.{j,t}sx'],
-      extends: [
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-      ],
     },
     {
       files: ['**/*.{cy|e2e}.{js,jsx,ts,tsx}'],
